a30682b8495146c049d747adf36ba03a769996ee
Murilo Francisco Matos <130021023+zLil0@users.noreply.github.com>
2024-03-31 21:21:06 -0300

    add pass

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -29,6 +29,11 @@ const userSchema = z.object({
             invalid_type_error: "Url do avatar deve ser um texto."
         })
         .url({ message: 'Url do avatar inválido.' }),
+    pass: z
+        .string({
+            required_error: "A senha é obrigatória",
+            invalid_type_error: "Senha deve ser um texto"
+        })
 })
 
 
@@ -37,44 +42,44 @@ const list = async () => {
 }
 
 const getUser = async (id) => {
-	return await prisma.user.findUnique({
-		where: {
-			id
-		}
-	})
+    return await prisma.user.findUnique({
+        where: {
+            id
+        }
+    })
 }
 
-const add = async (user) =>{
+const add = async (user) => {
     return await prisma.user.create({
-		data: user
-	})
+        data: user
+    })
 }
 
 const edit = async (updated) => {
     return await prisma.user.update({
-		where:{
-			id: updated.id
-		},
-		data: updated
-	})
+        where: {
+            id: updated.id
+        },
+        data: updated
+    })
 }
 
 const remove = async (id) => {
     return await prisma.user.delete({
-		where: {
-			id
-		}
-	})
+        where: {
+            id
+        }
+    })
 }
 
 const validateAdd = (user) => {
-    const partialUserSchema = userSchema.partial({id: true})
+    const partialUserSchema = userSchema.partial({ id: true })
     return partialUserSchema.safeParse(user)
 }
 
 const validateId = (user) => {
-    const partialUserSchema = userSchema.partial({name: true, avatar: true, email: true})
+    const partialUserSchema = userSchema.partial({ name: true, avatar: true, email: true })
     return partialUserSchema.safeParse(user)
 }
 
-export default { list, add, edit, remove, validateAdd, validateId, getUser}
+export default { list, add, edit, remove, validateAdd, validateId, getUser }
